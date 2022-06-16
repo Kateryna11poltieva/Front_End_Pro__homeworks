290b9c6e8a5ca87e9e2b234c3473df54003fdf77
Kateryna11poltieva <katerinapoltevaqoor@gmail.com>
2022-06-16 17:23:31 +0300

    aded folder with homework for lesson 1.

---
+++ poltievalesson1hw/js/index.js
@@ -0,0 +1,10 @@
+const userName = prompt('Enter yor name please');
+const userEmail = prompt ('Enter your email please');
+const userAge = prompt ('Enter your age please');
+const userPhone = prompt ('Enter your mobile phone please');
+
+alert ('welcom to the worl of happiness,' + ' ' + userName)
+
+const summery = userName  + userEmail  + userAge  + userPhone;
+
+console.log(summery);
