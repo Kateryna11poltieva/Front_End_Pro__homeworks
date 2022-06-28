3fc7342b16125a3597cda8133734544d3b6a4899
Kateryna11poltieva <katerinapoltevaqoor@gmail.com>
2022-06-28 13:56:59 +0300

    added first tusk in homework

---
+++ poltievaLesson4hw/js/firstTask.js
@@ -0,0 +1,9 @@
+let x = parseInt(prompt('Введіть будь ласка начальне значення'))
+
+let y = parseInt(prompt('Введіть будь ласка кінцеве значення'))
+
+for (let i = x; i < y; i++) {
+    if (i % 1 == 0) {
+        alert( i );
+    }
+}
